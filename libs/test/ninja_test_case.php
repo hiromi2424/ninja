a3e256d8472f021b8690bcc76fb76e14977975bb
hiromi2424 <tanaka2424@gmail.com>
2011-05-25 11:28:07 +0900

    override _isTest() in order to specify cases to run more easily and conveniently.

--- libs/test/ninja_test_case.php
+++ libs/test/ninja_test_case.php
@@ -7,6 +7,9 @@ abstract class NinjaTestCase extends FixtureLoaderTestCase {
 
 	public $plugin;
 
+	public $runOnly = null;
+	public $excludeCase = null;
+
 	public function __construct() {
 		if (!empty($_GET['plugin'])) {
 			$this->plugin = Inflector::humanize($_GET['plugin']) . '.';
@@ -15,6 +18,29 @@ abstract class NinjaTestCase extends FixtureLoaderTestCase {
 
 	public function _testFile($name) {
 		return TESTS . 'files' . DS . $name;
+	}
+
+	function _isTest($method) {
+		if ($isTest = parent::_isTest($method)) {
+			if ($this->runOnly !== null) {
+				$candidates = (array)$this->runOnly;
+			}
+
+			if ($this->excludeCase !== null) {
+				$excludes = (array)$this->excludeCase;
+			}
+
+			switch (true) {
+				case isset($candidates, $excludes):
+					return in_array($method, array_diff($candidates, $excludes));
+				case isset($candidates):
+					return in_array($method, $candidates);
+				case isset($excludes):
+					return !in_array($method, $excludes);
+			}
+		}
+
+		return $isTest;
 	}
 
 }
