dafa506f8926973903035b1a77c98b15b4f9116a
hiromi2424 <tanaka2424@gmail.com>
2011-06-22 04:44:29 +0900

    added MultiSlugRoute with test case

---
+++ libs/route/multi_slug_route.php
@@ -0,0 +1,356 @@
+<?php
+
+App::import('Lib', 'Ninja.route' . DS . 'NinjaRoute');
+
+class MultiSlugRoute extends NinjaRoute {
+
+	public static $defaultOptions = array(
+		'model' => null,
+		'cacheConfig' => 'default',
+		'identity' => ':alias',
+		'urlencode' => true,
+		'conditions' => array(),
+	);
+
+	public static $defaultSlugOptions = array(
+		'callbacks' => array(
+			'parse' => false,
+			'match' => false,
+		),
+	);
+
+	protected $_slugOptions = array();
+	protected $_models = array();
+
+	private $__map = null;
+	private $__writeMap = false;
+
+	protected static $_instances = array();
+
+	protected $_identity;
+
+	public function __construct($route, $defaults = array(), $options = array()) {
+
+		$defaultOptions = array(
+			'model' => isset($defaults['controller']) ? $defaults['controller'] : null,
+		) + self::$defaultOptions;
+		$options = Set::merge($defaultOptions, $options);
+
+		parent::__construct($route, $defaults, $options);
+
+		$this->_setSlugOptions($options);
+
+		$this->_identity = $this->getOption('identity');
+		self::$_instances[$this->_identity] = $this;
+
+	}
+
+	protected function _setSlugOptions() {
+		foreach (Set::normalize($this->options['slugs']) as $slug => $options) {
+			$defaultBySlug = array(
+				'display' => $slug,
+			);
+
+			$this->_slugOptions[$slug] = Set::merge(
+				array_intersect_key($this->options, array_flip(array('model', 'urlencode'))),
+				$defaultBySlug,
+				self::$defaultSlugOptions,
+				$options
+			);
+		}
+	}
+
+	public function getOption($name, $options = null) {
+		if ($options === null) {
+			$options = $this->options;
+		}
+
+		if (!isset($options[$name])) {
+			trigger_error(sprintf(__d('ninja', '%s option is not set', true), $name));
+			return null;
+		}
+
+		if (!is_string($options[$name]) || strpos($options[$name], ':') === false) {
+			return $options[$name];
+		}
+
+		$model = $this->_getModel($options['model']);
+		return $this->_insertParams($options[$name], $model);
+	}
+
+	protected function _getModel($model = null) {
+		if ($model === null) {
+			$model = $this->options['model'];
+		}
+
+		if ($model === null) {
+			trigger_error(__d('ninja', 'Detecting model failed. you should specify the model', true));
+			return null;
+		}
+
+		$name = Inflector::classify($model);
+		if (isset($this->_models[$name])) {
+			return $this->_models[$name];
+		}
+		return $this->_models[$name] = ClassRegistry::init($name);
+	}
+
+	public function match($params) {
+		if (!$this->_match($params)) {
+			return false;
+		}
+
+		if (isset($params[0])) {
+			$id = $params[0];
+			if (!($slugs = $this->mapped($id))) {
+				$data = $this->_lookup(array(
+					$this->_getModel()->escapeField() => $id,
+				));
+
+				if (empty($data)) {
+					return false;
+				}
+
+				$slugs = array();
+				foreach ($this->_slugOptions as $slug => $options) {
+					$alias = $this->_getModel($options['model'])->alias;
+					$field = $this->getOption('display', $options);
+					if (!isset($data[$alias][$field])) {
+						return false;
+					}
+					$slugs[$slug] = $data[$alias][$field];
+				}
+
+				$this->map($id, $slugs);
+			}
+
+			foreach ($this->_slugOptions as $slug => $options) {
+				$params[$slug] = $slugs[$slug];
+
+				$callbacks = $this->getOption('callbacks', $options);
+				if ($callbacks['match'] !== false) {
+					$params[$slug] = $callbacks['match']($params[$slug]);
+				}
+
+				if ($urlencode = $this->getOption('urlencode', $options)) {
+					$encodeMethod = $urlencode === true ? 'rawurlencode' : $urlencode;
+					$params[$slug] = $encodeMethod($params[$slug]);
+				}
+			}
+
+			unset($params[0]);
+			$params = $this->_normalizePass($params);
+			$url = parent::match($params);
+
+			return $url;
+		}
+
+		return false;
+
+	}
+
+	public function parse($url) {
+		$params = parent::parse($url); // get cake standard
+
+		if (empty($params) || !$this->_match($params)) {
+			return false;
+		}
+
+		$slugs = array();
+		foreach ($this->_slugOptions as $slug => $options) {
+			if (!isset($params[$slug])) {
+				return false;
+			}
+
+			$slugs[$slug] = $params[$slug];
+			unset($params[$slug]);
+
+			$callbacks = $this->getOption('callbacks', $options);
+			if ($callbacks['parse'] !== false) {
+				$slugs[$slug] = $callbacks['parse']($slugs[$slug]);
+			}
+		}
+
+		if (!($id = $this->mapped($slugs))) {
+			$result = $this->_lookup($this->_generateScope($slugs));
+			if (!$result) {
+				return false;
+			}
+
+			$id = $result[$this->_getModel()->alias][$this->_getModel()->primaryKey];
+			$this->map($id, $slugs);
+		}
+
+		if (empty($params['pass'])) {
+			$params['pass'] = array();
+		}
+		$params['pass'][] = $id;
+
+		return $params;
+
+	}
+
+	protected function _generateScope($slugs) {
+		$scope = array();
+		foreach ($this->_slugOptions as $slug => $options) {
+			$scope[$this->_getModel($options['model'])->escapeField($this->getOption('display', $options))] = $slugs[$slug];
+		}
+		return $scope;
+	}
+
+	protected function _lookup($scope = array()) {
+		$query = Set::merge($this->generateQuery(), array('conditions' => $scope));
+		$model = $this->_getModel();
+
+		if (!($attached = $model->Behaviors->attached('Containable'))) {
+			$model->Behaviors->attach('Containable');
+		}
+
+		$result = $model->find('first', $query);
+
+		if (!$attached) {
+			$model->Behaviors->detach('Containable');
+		}
+
+		return $result;
+	}
+
+	public function mapped($from) {
+		$this->_loadMap();
+
+		if (is_string($from) || is_int($from)) {
+			if (array_key_exists($from, $this->__map)) {
+				return $this->__map[$from];
+			}
+		} elseif (is_array($from)) {
+			if ($id = array_search($from, $this->__map)) {
+				return $id;
+			}
+		}
+
+		return false;
+	}
+
+	public function map($id, $slugs) {
+		$this->_loadMap();
+
+		$this->__map[$id] = $slugs;
+		$this->_writeMap();
+	}
+
+	protected function _writeMap() {
+		// saves parameters for use in destruct(after model destructing, call of model might not work)
+		if ($this->__writeMap === false) {
+			$this->__writeMap = array(
+				'cacheConfig' => $this->getOption('cacheConfig'),
+				'cacheName' => $this->cacheName(),
+			);
+		}
+	}
+
+	protected function _loadMap() {
+		if ($this->__map === null) {
+			// $this->elapsed = microtime(true);
+			$this->__map = Cache::read($this->cacheName(), $this->getOption('cacheConfig'));
+			// $this->elapsed = microtime(true) - $this->elapsed;
+			if (!$this->__map) {
+				$this->__map = array();
+			}
+		}
+	}
+
+	public function generateQuery() {
+		$baseModel = Inflector::classify($this->options['model']);
+		$fields = array($this->_getModel($baseModel)->escapeField());
+		$contain = array();
+		foreach ($this->_slugOptions as $options) {
+			$model = Inflector::classify($options['model']);
+			if ($model !== $baseModel && !in_array($model, $contain)) {
+				$contain[] = $model;
+			}
+
+			$model = $this->_getModel($model);
+			$fields[] = $model->escapeField($this->getOption('display', $options));
+		}
+
+		$fields = array_values(array_unique($fields));
+
+		$conditions = $this->getOption('conditions');
+
+		return compact('fields', 'contain', 'conditions');
+	}
+
+	public function cacheName() {
+		return Inflector::tableize($this->_identity);
+	}
+
+	public function __destruct() {
+		if ($this->__map !== null && $this->__writeMap !== false) {
+			Cache::write($this->__writeMap['cacheName'], $this->__map, $this->__writeMap['cacheConfig']);
+		}
+
+		if (isset(self::$_instances[$this->_identity]) && self::$_instances[$this->_identity] === $this) {
+			unset(self::$_instances[$this->_identity]);
+		}
+	}
+
+	public static function getInstance($identity) {
+		if (isset(self::$_instances[$identity])) {
+			return self::$_instances[$identity];
+		}
+	}
+
+
+	public static function getCacheName($identity) {
+		if (isset(self::$_instances[$identity])) {
+			return self::$_instances[$identity]->cacheName();
+		}
+	}
+
+	public static function readCache($identity) {
+		if (isset(self::$_instances[$identity])) {
+			$instance = self::$_instances[$identity];
+			$instance->_loadMap();
+			return $instance->__map;
+		}
+	}
+
+	public static function storeCache($identity, $data) {
+		if (isset(self::$_instances[$identity])) {
+			$instance = self::$_instances[$identity];
+			$instance->_loadMap();
+			$instance->__map = $data;
+			$instance->_writeMap();
+			return true;
+		}
+	}
+
+	public static function clearCache($identity) {
+		if (isset(self::$_instances[$identity])) {
+			$instance = self::$_instances[$identity];
+			$instance->__map = array();
+			$instance->__writeMap = false;
+			return Cache::delete($instance->cacheName(), $instance->getOption('cacheConfig'));
+		}
+	}
+
+	public static function clearCacheAll() {
+		if (empty(self::$_instances)) {
+			return null;
+		}
+
+		$result = true;
+		foreach (array_keys(self::$_instances) as $identity) {
+			if (!self::clearCache($identity)) {
+				$result = false;
+			}
+		}
+
+		return $result;
+	}
+
+	public static function flush() {
+		self::$_instances = array();
+	}
+
+}
---
+++ tests/cases/libs/route/multi_slug_route.test.php
@@ -0,0 +1,354 @@
+<?php
+
+App::import('Core', 'Router');
+App::import('Lib', 'Ninja.route' . DS . 'MultiSlugRoute');
+App::import('Lib', 'Ninja.test' . DS . 'NinjaTestCase');
+
+class MultiSlugRouteTestPost extends CakeTestModel {
+
+	public $name = 'MultiSlugRouteTestPost';
+	public $alias = 'Post';
+	public $useTable = 'posts';
+
+	public $belongsTo = 'Author';
+
+}
+
+class MultiSlugRouteTest extends NinjaTestCase {
+
+	protected $_routingBackup;
+	protected $_cacheBackup;
+
+	public $fixtures = array(
+		'core.post',
+		'core.author',
+	);
+
+	public $excludeCase = 'testScalability';
+
+	public function startCase() {
+		parent::startCase();
+		$this->_routingBackup = Configure::read('Routing');
+		$this->_cacheBackup = Configure::read('Cache');
+
+		Configure::write('Routing', null);
+		Configure::write('Cache.disable', false);
+
+		ClassRegistry::init(array('class' => 'MultiSlugRouteTestPost', 'alias' => 'Post'));
+	}
+
+	public function endCase() {
+		Configure::write('Routing', $this->_routingBackup);
+		Configure::write('Cache', $this->_cacheBackup);
+		parent::endCase();
+	}
+
+	public function endTest($method = null) {
+		MultiSlugRoute::clearCacheAll();
+		MultiSlugRoute::flush();
+		parent::endTest($method);
+	}
+
+	protected function _createInstance($route = null, $defaults = null, $options = null) {
+		$defaultRoute = '/post/:user/:title';
+		$defaultDefaults = array(
+			'controller' => 'posts',
+			'action' => 'view',
+		);
+		$defualtOptions = array(
+			'slugs' => array(
+				'user' => array(
+					'model' => 'Author',
+				),
+				'title',
+			),
+		);
+
+		$route = $route === null ? $defaultRoute : $route;
+		$defaults = $defaults === null ? $defaultDefaults : $defaults;
+		$options = $options === null ? $defualtOptions : $options;
+
+		return new MultiSlugRoute($route, $defaults, $options);
+	}
+
+	public function testMatch() {
+		$route = $this->_createInstance();
+
+		$result = $route->match(array('controller' => 'posts', 'action' => 'index'));
+		$this->assertFalse($result);
+
+		$result = $route->match(array('controller' => 'author', 'action' => 'view', 1));
+		$this->assertFalse($result);
+
+		$result = $route->match(array('controller' => 'posts', 'action' => 'view'));
+		$this->assertFalse($result);
+
+		$result = $route->match(array('controller' => 'posts', 'action' => 'view', -1));
+		$this->assertFalse($result);
+
+		$result = $route->match(array('controller' => 'posts', 'action' => 'view', 1));
+		$this->assertEqual($result, sprintf('/post/%s/%s', rawurlencode('mariano'), rawurlencode('First Post')));
+
+		// cached
+		$result = $route->match(array('controller' => 'posts', 'action' => 'view', 1));
+		$this->assertEqual($result, sprintf('/post/%s/%s', rawurlencode('mariano'), rawurlencode('First Post')));
+
+		ClassRegistry::init('Post')->saveAll(array(
+			'Post' => array(
+				'id' => 10,
+				'title' => 'New Post',
+				'body' => 'body',
+				'published' => 'Y',
+			),
+			'Author' => array(
+				'id' => 10,
+				'user' => 'Hiromi',
+				'password' => 'dummy',
+			),
+		));
+
+		$result = $route->match(array('controller' => 'posts', 'action' => 'view', 10));
+		$this->assertEqual($result, sprintf('/post/%s/%s', rawurlencode('Hiromi'), rawurlencode('New Post')));
+
+
+	}
+
+	public function testParse() {
+		$route = $this->_createInstance();
+
+		$result = $route->parse('/invalidUrl');
+		$this->assertFalse($result);
+
+		$result = $route->parse('/post/invaliduser/invalidtitle');
+		$this->assertFalse($result);
+
+		// only author is valid
+		$result = $route->parse('/post/mariano/invalidtitle');
+		$this->assertFalse($result);
+
+		// only title is valid
+		$result = $route->parse('/post/invaliduser/First Post');
+		$this->assertFalse($result);
+
+		$result = $route->parse('/post/mariano/First Post');
+		$this->assertEqual($result['controller'], 'posts');
+		$this->assertEqual($result['action'], 'view');
+		$this->assertEqual($result['pass'], array(1));
+
+		// cached
+		$result = $route->parse('/post/mariano/First Post');
+		$this->assertEqual($result['controller'], 'posts');
+		$this->assertEqual($result['action'], 'view');
+		$this->assertEqual($result['pass'], array(1));
+
+		ClassRegistry::init('Post')->saveAll(array(
+			'Post' => array(
+				'id' => 10,
+				'title' => 'New Post',
+				'body' => 'body',
+				'published' => 'Y',
+			),
+			'Author' => array(
+				'id' => 10,
+				'user' => 'Hiromi',
+				'password' => 'dummy',
+			),
+		));
+
+		$result = $route->parse('/post/Hiromi/New Post');
+		$this->assertEqual($result['controller'], 'posts');
+		$this->assertEqual($result['action'], 'view');
+		$this->assertEqual($result['pass'], array(10));
+
+	}
+
+	public function testMapping() {
+		$route = $this->_createInstance();
+		$route->parse('/post/mariano/First Post');
+		$mapParse = MultiSlugRoute::readCache('Post');
+
+		MultiSlugRoute::clearCacheAll();
+
+		$route = $this->_createInstance();
+		$route->match(array('controller' => 'posts', 'action' => 'view', 1));
+		$mapMatch = MultiSlugRoute::readCache('Post');
+		$this->assertEqual($mapParse, $mapMatch);
+	}
+
+	public function testCallback() {
+		$route = $this->_createInstance('/post/:title/:user', null, array(
+			'slugs' => array(
+				'title',
+				'user' => array(
+					'model' => 'Author',
+					'callbacks' => array(
+						'match' => function($value) {
+							return 'author;' . $value;
+						},
+						'parse' => function($slug) {
+							return preg_replace('/^author;/', '', $slug);
+						},
+					),
+				),
+			),
+		));
+
+		$result = $route->parse('/post/First Post/author;mariano');
+		$this->assertEqual($result['controller'], 'posts');
+		$this->assertEqual($result['action'], 'view');
+		$this->assertEqual($result['pass'], array(1));
+
+		$result = $route->match(array('controller' => 'posts', 'action' => 'view', 1));
+		$this->assertEqual($result, sprintf('/post/%s/%s', rawurlencode('First Post'), rawurlencode('author;mariano')));
+
+	}
+
+	public function testGreedy() {
+		$route = $this->_createInstance('/post/:user/:title/*');
+
+		$result = $route->parse('/post/mariano/First Post/2');
+		$this->assertEqual($result['controller'], 'posts');
+		$this->assertEqual($result['action'], 'view');
+		$this->assertEqual($result['pass'], array(1));
+		$this->assertEqual($result['_args_'], 2);
+
+		$result = $route->match(array('controller' => 'posts', 'action' => 'view', 1, 2));
+		$this->assertEqual($result, sprintf('/post/%s/%s/2', rawurlencode('mariano'), rawurlencode('First Post'), rawurlencode('2')));
+
+	}
+
+	public function testGenerateQuery() {
+		$route = $this->_createInstance();
+
+		$result = $route->generateQuery();
+		$expected = array(
+			'fields' => array(
+				ClassRegistry::init('Post')->escapeField('id'),
+				ClassRegistry::init('Author')->escapeField('user'),
+				ClassRegistry::init('Post')->escapeField('title'),
+			),
+			'contain' => array(
+				'Author',
+			),
+			'conditions' => array(
+			),
+		);
+		$this->assertEqual($result, $expected);
+	}
+
+	public function testCacheControll() {
+
+		$this->assertNull(MultiSlugRoute::clearCacheAll());
+		$this->assertNull(MultiSlugRoute::clearCache('Post'));
+		$this->assertNull(MultiSlugRoute::readCache('Post'));
+		$this->assertNull(MultiSlugRoute::storeCache('Post', 'data'));
+
+		$route = $this->_createInstance();
+
+		$result = MultiSlugRoute::getCacheName('Post');
+		$this->assertEqual($result, 'posts');
+
+		$route->parse('/post/mariano/First Post');
+		$route = $this->_createInstance();
+
+		$result = MultiSlugRoute::readCache('Post');
+		$id = key($result);
+		$slugs = current($result);
+		$this->assertEqual($id, 1);
+		$this->assertEqual($slugs['user'], 'mariano');
+		$this->assertEqual($slugs['title'], 'First Post');
+
+		$this->assertTrue(MultiSlugRoute::storeCache('Post', 'test'));
+		$this->assertEqual(MultiSlugRoute::readCache('Post'), 'test');
+		$this->assertTrue(MultiSlugRoute::clearCache('Post'));
+		$this->assertEqual(MultiSlugRoute::readCache('Post'), array());
+		$this->assertFalse(MultiSlugRoute::clearCache('Post'));
+		$this->assertFalse(MultiSlugRoute::clearCacheAll());
+	}
+
+	public function testScalability() {
+		$route = $this->_createInstance();
+
+		$Post = ClassRegistry::init('Post');
+		$Author = ClassRegistry::init('Author');
+		$Post->deleteAll(true);
+		$Author->deleteAll(true);
+
+		$recordUnit = 1000;
+		$unitAmount = 100;
+		$recordsCount = $recordUnit * $unitAmount;
+		var_dump("testing $recordsCount records:");
+
+		$start = microtime(true);
+		for ($i = 0; $i < $unitAmount; $i++) {
+			$save = array();
+			for ($j = 1; $j <= $recordUnit; $j++) {
+				$n = $i * $recordUnit + $j;
+				$save['Author'][$n] = "($n, 'user$n', 'password$n', '2011-11-11 11:11:11', '2011-11-11 11:11:11')";
+				$save['Post'][$n] = "($n, 'Post$n', 'body$n', 'Y', $n, '2011-11-11 11:11:11', '2011-11-11 11:11:11')";
+			}
+			$Author->getDataSource()->insertMulti('authors', array('id', 'user', 'password', 'created', 'updated'), $save['Author']);
+			$Post->getDataSource()->insertMulti('posts', array('id', 'title', 'body', 'published', 'author_id', 'created', 'updated'), $save['Post']);
+		}
+		$end = microtime(true) - $start;
+		var_dump(sprintf('inserting records took %.1f msec', 1000.0 * $end));
+
+		$start = microtime(true);
+		$result = $route->parse('/post/user100/Post100');
+		$end = microtime(true) - $start;
+		var_dump(sprintf('non cached parsing took %.1f msec', 1000.0 * $end));
+
+		$this->assertEqual($result['controller'], 'posts');
+		$this->assertEqual($result['action'], 'view');
+		$this->assertEqual($result['pass'], array(100));
+
+		$start = microtime(true);
+		$result = $route->parse('/post/user100/Post100');
+		$end = microtime(true) - $start;
+		var_dump(sprintf('after 1 cached parsing took %.1f msec', 1000.0 * $end));
+
+		MultiSlugRoute::clearCacheAll();
+		/*
+		$start = microtime(true);
+		for ($i = 1; $i <= $recordsCount; $i++) {
+			$startIndivisual = microtime(true);
+			$route->parse("/post/user$i/Post$i");
+			if ($i === 2 || $i === $n) {
+				var_dump(sprintf($i . ': %.1f msec', 1000.0 * (microtime(true) - $startIndivisual)));
+			}
+		}
+		$end = microtime(true) - $start;
+		var_dump(sprintf('%.1f msec', 1000.0 * $end));
+		var_dump(sprintf('average %.1f msec', 1000.0 * $end / (float)$n));
+		*/
+
+		$start = microtime(true);
+		for ($i = 1; $i <= $recordsCount; $i++) {
+			$route->map(
+				"/post/user$i/Post$i",
+				array(
+					'controller' => 'posts',
+					'action' => 'view',
+					'pass' => array($i),
+					'named' => array(),
+				)
+			);
+		}
+		$end = microtime(true) - $start;
+		var_dump(sprintf('mapping took %.1f msec', 1000.0 * $end));
+
+		$route = $this->_createInstance();
+
+		$start = microtime(true);
+		$result = $route->parse('/post/user100/Post100');
+		$end = microtime(true) - $start;
+		var_dump(sprintf('after all cached parsing with loading cache took %.1f msec', 1000.0 * $end));
+		var_dump(sprintf('loading cache took %.1f msec', 1000.0 * $route->elapsed));
+
+		$start = microtime(true);
+		$result = $route->parse('/post/user100/Post100');
+		$end = microtime(true) - $start;
+		var_dump(sprintf('after all cached parsing took %.1f msec', 1000.0 * $end));
+	}
+
+}
