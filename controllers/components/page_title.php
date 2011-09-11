4a2b61d2cac9c06534ab2626404a580c5d1e7932
hiromi2424 <tanaka2424@gmail.com>
2011-09-11 16:33:58 +0900

    refactoring PageTitle component

--- controllers/components/page_title.php
+++ controllers/components/page_title.php
@@ -2,27 +2,50 @@
 
 class PageTitleComponent extends Object {
 
+	public $controller;
+
+	public function initialize($controller, $settings = array()) {
+		$this->controller = $controller;
+	}
+
 	public function beforeRender($controller) {
+
+		$title = $this->getTitle();
+		if (!$title) {
+			return;
+		}
+
+		$this->controller->set('title_for_layout', $title);
+
+	}
+
+	public function getTitle($options = array()) {
+		$options += array(
+			'action' => $this->controller->action,
+			'prefix' => true,
+			'suffix' => true,
+		);
 
 		if (isset($controller->pageTitle)) {
 			$title = $controller->pageTitle;
-		} elseif (isset($controller->pageTitles) && array_key_exists($controller->action, $controller->pageTitles)) {
-			$title = $controller->pageTitles[$controller->action];
-		} elseif (isset($controller->defaultTitle)) {
-			$title = $controller->defaultTitle;
+		} elseif (isset($this->controller->pageTitles) && array_key_exists($options['action'], $this->controller->pageTitles)) {
+			$title = $this->controller->pageTitles[$options['action']];
+		} elseif (isset($this->controller->defaultTitle)) {
+			$title = $this->controller->defaultTitle;
 		} else {
 			return;
 		}
 
-		if (isset($controller->titlePrefix)) {
-			$title = $controller->titlePrefix . $title;
+		if ($options['prefix'] && isset($this->controller->titlePrefix)) {
+			$title = $this->controller->titlePrefix . $title;
 		}
 
-		if (isset($controller->titleSuffix)) {
-			$title .= $controller->titleSuffix;
+		if ($options['suffix'] && isset($this->controller->titleSuffix)) {
+			$title .= $this->controller->titleSuffix;
 		}
 
-		$controller->set('title_for_layout', $title);
+		return $title;
 
 	}
+
 }
