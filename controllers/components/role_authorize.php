8a0c4fa68c3277d14daa56219823eff214266ea5
hiromi2424 <tanaka2424@gmail.com>
2011-09-14 06:04:10 +0900

    refactoring RoleAuthorize component and added test case for it

--- controllers/components/role_authorize.php
+++ controllers/components/role_authorize.php
@@ -2,28 +2,41 @@
 
 class RoleAuthorizeComponent extends Object {
 
+	public $auth = 'Auth';
+	public $configName = 'Role.level';
+	public $levelField = 'Group.level';
+	public $usePrefix = true;
+
 	public $controller;
 
-	public function initialize($controller, $settings) {
+	public function initialize($controller, $settings = array()) {
 		$this->controller = $controller;
+		$this->_set($settings);
 	}
 
 	public function authorize() {
 
-		if (!empty($this->controller->params['prefix'])) {
-			$requireAuth = $this->controller->params['prefix'];
-		} elseif (isset($this->controller->requireAuth[$this->controller->action]) && is_array($this->controller->requireAuth)) {
-			$requireAuth = $this->controller->requireAuth[$this->controller->action];
-		} elseif (isset($this->controller->requireAuth)) {
+		if (!empty($this->controller->requireAuth) && !is_array($this->controller->requireAuth)) {
 			$requireAuth = $this->controller->requireAuth;
+		} elseif (!empty($this->controller->requireAuth) && is_array($this->controller->requireAuth) && !empty($this->controller->requireAuth[$this->controller->action])) {
+			$requireAuth = $this->controller->requireAuth[$this->controller->action];
+		} elseif ($this->usePrefix && !empty($this->controller->params['prefix'])) {
+			$requireAuth = $this->controller->params['prefix'];
 		}
 
-		if (isset($requireAuth) && Configure::read('Role.level.' . $requireAuth) > $this->controller->Auth->user('Group.level')) {
-			return false;
+		if (isset($requireAuth)) {
+			$config = Configure::read($this->configName);
+			if (array_key_exists($requireAuth, $config)) {
+				return $config[$requireAuth] <= $this->controller->{$this->auth}->user($this->levelField);
+			}
 
 		}
 
 		return true;
+	}
+
+	public function isAuthorized() {
+		return $this->authorize();
 	}
 
 }
---
+++ tests/cases/controllers/components/role_authorize.test.php
@@ -0,0 +1,72 @@
+<?php
+/* RoleAuthorize Test cases generated on: 2011-09-12 23:38:00 : 1315838280*/
+
+App::import('Lib', 'Ninja.test' . DS . 'NinjaComponentTestCase');
+App::import('Component', 'Auth');
+
+Mock::generate('AuthComponent');
+
+class RoleAuthorizeComponentTestCase extends NinjaComponentTestCase {
+
+	public $fixtures = false;
+
+	protected $_configBackup;
+
+	protected $_testConfigName = '__RoleAuthorizeTestConfig';
+	protected $_currentLevel = 200;
+
+	public function startTest($method = null) {
+		parent::startTest($method);
+		$this->_configBackup = Configure::read($this->_testConfigName);
+		Configure::write($this->_testConfigName, array(
+			'member' => 200,
+			'moderator' => 400,
+			'admin' => 800,
+		));
+		$this->Controller->Auth = new MockAuthComponent;
+		$this->Controller->Auth->setReturnReference('user', $this->_currentLevel);
+	}
+
+	public function endTest($method = null) {
+		Configure::delete($this->_testConfigName);
+		if ($this->_configBackup !== false) {
+			Configure::write($this->_testConfigName, $this->_configBackup);
+		}
+		parent::endTest($method);
+	}
+
+	public function testAuthorize() {
+		$this->assertTrue($this->RoleAuthorize->authorize());
+
+		$this->Controller->params['prefix'] = 'admin';
+		$this->assertFalse($this->RoleAuthorize->authorize());
+		$this->Controller->params['prefix'] = 'member';
+		$this->assertTrue($this->RoleAuthorize->authorize());
+		unset($this->Controller->params['prefix']);
+
+		$this->Controller->requireAuth = 'admin';
+		$this->assertFalse($this->RoleAuthorize->authorize());
+		$this->Controller->requireAuth = 'member';
+		$this->assertTrue($this->RoleAuthorize->authorize());
+		unset($this->Controller->requireAuth);
+
+		$this->Controller->requireAuth = array('test_action' => 'admin');
+		$this->assertFalse($this->RoleAuthorize->authorize());
+		$this->Controller->requireAuth = array('test_action' => 'member');
+		$this->assertTrue($this->RoleAuthorize->authorize());
+
+		$this->Controller->requireAuth = 'member';
+		$this->_currentLevel = 199;
+		$this->assertFalse($this->RoleAuthorize->authorize());
+		$this->_currentLevel = 200;
+		$this->assertTrue($this->RoleAuthorize->authorize());
+		$this->_currentLevel = 200.0;
+		$this->assertTrue($this->RoleAuthorize->authorize());
+		$this->_currentLevel = '200';
+		$this->assertTrue($this->RoleAuthorize->authorize());
+		$this->_currentLevel = 'hoge';
+		$this->assertFalse($this->RoleAuthorize->authorize());
+
+	}
+
+}
