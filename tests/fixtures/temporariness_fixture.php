548c4330dfc8008f4684fa645c5f654d1a03bed8
hiromi2424 <hiromi2424@gmail.com>
2012-01-19 19:49:20 +0900

    added database cache engine

---
+++ tests/fixtures/temporariness_fixture.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class TemporarinessFixture extends NinjaTestFixture {
+
+	public $name = 'Temporariness';
+
+	public $fields = array(
+		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'value' => array('type' => 'binary', 'null' => false, 'default' => NULL),
+		'expires' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'index'),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'expires' => array('column' => 'expires', 'unique' => 0))
+	);
+
+}
