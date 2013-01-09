8bf32c38db9755022848301ce1e8f69656079a80
hiromi2424 <tanaka2424@gmail.com>
2013-01-09 13:45:46 +0900

    fixed RoleAuthorize test case

--- Test/Case/Controller/Component/Auth/RoleAuthorizeTest.php
+++ Test/Case/Controller/Component/Auth/RoleAuthorizeTest.php
@@ -1,13 +1,15 @@
 <?php
 /* RoleAuthorize Test cases generated on: 2011-09-12 23:38:00 : 1315838280*/
 
-App::uses('NinjaComponentTestCase', 'Ninja.TestSuite');
+App::uses('Controller', 'Controller');
+App::uses('CakeRequest', 'Network');
+App::uses('NinjaTestCase', 'Ninja.TestSuite');
 App::import('Component', 'Auth');
 
-App::uses('RoleAuthorizeComponent', 'Ninja.Controller/Component/Auth');
-class_exists('RoleAuthorizeComponent'); // loading
+App::uses('RoleAuthorize', 'Ninja.Controller/Component/Auth');
+class_exists('RoleAuthorize'); // loading
 
-class RoleAuthorizeComponentTestCase extends NinjaComponentTestCase {
+class RoleAuthorizeTestCase extends NinjaTestCase {
 
 	public $fixtures = false;
 
@@ -15,6 +17,20 @@ class RoleAuthorizeComponentTestCase extends NinjaComponentTestCase {
 
 	protected $_testConfigName = '__RoleAuthorizeTestConfig';
 	protected $_currentLevel = 200;
+
+	protected function _instantiate() {
+		$this->Controller = new Controller(new CakeRequest());
+		$this->Controller->constructClasses();
+		$this->Controller->action = 'test_action';
+		$this->RoleAuthorize = new RoleAuthorize($this->Controller->Components);
+	}
+
+	public function tearDown() {
+		unset($this->RoleAuthorize);
+		unset($this->Controller);
+		ClassRegistry::flush();
+		parent::tearDown();
+	}
 
 	public function startTest($method = null) {
 		parent::startTest($method);
@@ -27,9 +43,17 @@ class RoleAuthorizeComponentTestCase extends NinjaComponentTestCase {
 		$this->Controller->Auth = $this->getMock('AuthComponent', array('user'), array(new ComponentCollection));
 		$this->Controller->Auth->staticExpects($this->any())
 			->method('user')
-			->will($this->returnCallback(array($this, 'currentLevel')));
+			->will($this->returnValue(1));
 
 		$this->RoleAuthorize->configName = $this->_testConfigName;
+
+		$Group = $this->getMock('Model');
+		$Group->expects($this->any())
+			->method('field')
+			->will($this->returnCallback(array($this, 'currentLevel')));
+		$Group->alias = 'Group';
+		ClassRegistry::addObject('Group', $Group);
+		ClassRegistry::map('Group', 'Group');
 	}
 
 	public function currentLevel() {
@@ -45,36 +69,38 @@ class RoleAuthorizeComponentTestCase extends NinjaComponentTestCase {
 	}
 
 	public function testAuthorize() {
-		$this->assertTrue($this->RoleAuthorize->authorize());
+		$user = array(); // mock
+
+		$this->assertTrue($this->RoleAuthorize->authorize($user, $this->Controller->request));
 
 		$this->Controller->request->params['prefix'] = 'admin';
-		$this->assertFalse($this->RoleAuthorize->authorize());
+		$this->assertFalse($this->RoleAuthorize->authorize($user, $this->Controller->request));
 		$this->Controller->request->params['prefix'] = 'member';
-		$this->assertTrue($this->RoleAuthorize->authorize());
+		$this->assertTrue($this->RoleAuthorize->authorize($user, $this->Controller->request));
 		unset($this->Controller->request->params['prefix']);
 
 		$this->Controller->requireAuth = 'admin';
-		$this->assertFalse($this->RoleAuthorize->authorize());
+		$this->assertFalse($this->RoleAuthorize->authorize($user, $this->Controller->request));
 		$this->Controller->requireAuth = 'member';
-		$this->assertTrue($this->RoleAuthorize->authorize());
+		$this->assertTrue($this->RoleAuthorize->authorize($user, $this->Controller->request));
 		unset($this->Controller->requireAuth);
 
 		$this->Controller->requireAuth = array('test_action' => 'admin');
-		$this->assertFalse($this->RoleAuthorize->authorize());
+		$this->assertFalse($this->RoleAuthorize->authorize($user, $this->Controller->request));
 		$this->Controller->requireAuth = array('test_action' => 'member');
-		$this->assertTrue($this->RoleAuthorize->authorize());
+		$this->assertTrue($this->RoleAuthorize->authorize($user, $this->Controller->request));
 
 		$this->Controller->requireAuth = 'member';
 		$this->_currentLevel = 199;
-		$this->assertFalse($this->RoleAuthorize->authorize());
+		$this->assertFalse($this->RoleAuthorize->authorize($user, $this->Controller->request));
 		$this->_currentLevel = 200;
-		$this->assertTrue($this->RoleAuthorize->authorize());
+		$this->assertTrue($this->RoleAuthorize->authorize($user, $this->Controller->request));
 		$this->_currentLevel = 200.0;
-		$this->assertTrue($this->RoleAuthorize->authorize());
+		$this->assertTrue($this->RoleAuthorize->authorize($user, $this->Controller->request));
 		$this->_currentLevel = '200';
-		$this->assertTrue($this->RoleAuthorize->authorize());
+		$this->assertTrue($this->RoleAuthorize->authorize($user, $this->Controller->request));
 		$this->_currentLevel = 'hoge';
-		$this->assertFalse($this->RoleAuthorize->authorize());
+		$this->assertFalse($this->RoleAuthorize->authorize($user, $this->Controller->request));
 
 	}
 
